7754ca844b372743f478790991b99a3adbb29c40
zimuxh <zimuxh@gmail.com>
2014-07-28 23:54:47 +0800

    commit

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 
 python project
 主要作为自己练习的一个项目。
+bootstrap
+http://www.bootcss.com/
+http://v3.bootcss.com/components/#nav-dropdowns
--- pb/src/pb/pb/urls.py
+++ pb/src/pb/pb/urls.py
@@ -14,6 +14,10 @@ urlpatterns = patterns('',
 
     (r'^js/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.JS_STATIC_PATH}),
     
+    (r'^css/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.CSS_STATIC_PATH}),
+    
+    (r'^html/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.HTML_STATIC_PATH}),
+    
     url(r'^admin/', include(admin.site.urls)),
                        
     url(r'^index$', 'pb.views.index'),
