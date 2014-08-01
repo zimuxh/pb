80f52ef4493d67050b16f85083e72c43b4c991a9
zimuxh <zimuxh@gmail.com>
2014-08-01 22:30:32 +0800

    commit

--- pb/src/pb/pb/settings.py
+++ pb/src/pb/pb/settings.py
@@ -83,7 +83,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = BASE_DIR + '/static/'
 #
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'static/html/'),
